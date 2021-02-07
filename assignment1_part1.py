f27ea4d76f66c425eaf5db3bd56f9b840fe844d7
Monmon Saha <saham2@upmc.edu>
2021-02-07 17:32:34 -0500

    updated

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -17,7 +17,7 @@ def testListDivide():
     assert listDivide([2,4,6,8,10]) == 5
     assert listDivide([30, 54, 63,98, 100], divide=10) == 2
     assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 6   
+    assert listDivide([1,2,3,4,5], 1) == 5   
     print("All assertions work")        
   except AssertionError as ex:    
     raise ListDivideException("An exception occurred") from ex
