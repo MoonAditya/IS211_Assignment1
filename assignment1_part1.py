2a682d0dc61c4e120149078635baf51e68e1b56c
Monmon Saha <saham2@upmc.edu>
2021-02-07 17:29:44 -0500

    updated all

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,19 +1,28 @@
+ ## Final version of Assignment 1
+class ListDivideException(Exception):
+  """ raising ListDivideException when the listDivide function  """
+  pass
 
-def listDivide(numbers, divide):
-    """
-    The function returns the number of elements in the numbers list that are divisibleby divide
-    """
-    pass
+
+def listDivide(numbers, divide=2):
+    count = 0
+    for eachNum in numbers :
+      if (eachNum % divide) == 0:
+        count += 1
+    return count
 
 def testListDivide():
-    """
-    Test listDivide
-    """
+  try:
     assert listDivide([1,2,3,4,5]) == 2
     assert listDivide([2,4,6,8,10]) == 5
     assert listDivide([30, 54, 63,98, 100], divide=10) == 2
     assert listDivide([]) == 0
-    assert listDivide([1,2,3,4,5], 1) == 5
+    assert listDivide([1,2,3,4,5], 1) == 6   
+    print("All assertion work")        
+  except AssertionError as ex:    
+    raise ListDivideException("An exception occurred") from ex
+    
     
 if __name__ == "__main__":
-    testListDivide()
+   testListDivide()
+
--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,10 +1,17 @@
-
+# Final version of Assignment 1, part 2
 class Book:
     def __init(self, author, title):
-        pass
-    
-    def display(self):
-        pass
+      self.author = author
+      self.title = title
+
+    def display(self, title, author):  
+      msg = "'{}', written by {}"
+      print(msg.format(title, author))
+
         
-if __name__ == "__main__":
-    pass
+if __name__ == "__main__":  
+  title = ['Of Mice and Men', 'To Kill a Mockingbird']
+  author = ['John Steinbeck', 'Harper Lee']
+  test = Book()
+  test.display(title[0], author[0])
+  test.display(title[1], author[1])
